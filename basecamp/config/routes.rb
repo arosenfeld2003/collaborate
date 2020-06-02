26ae806ab2d886abde694f793774ebf3f6940277
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-06-01 22:51:15 -0700

    added projects forms

--- basecamp/config/routes.rb
+++ basecamp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :projects
   devise_for :users
 
   root 'welcome#index'
--- basecamp/db/schema.rb
+++ basecamp/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_012530) do
+ActiveRecord::Schema.define(version: 2020_06_02_052916) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
