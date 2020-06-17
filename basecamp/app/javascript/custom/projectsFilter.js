92ebf4e0ccbf98bc3bdaf4abc49af31a11f53b45
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-06-16 21:10:47 -0700

    filter by conditional class names incomplete

---
+++ basecamp/app/javascript/custom/projectsFilter.js
@@ -0,0 +1,12 @@
+(function() {
+  window.onload = function() {
+    const projectsBtns = document.querySelectorAll('.projects-filter-btn');
+    const projectsContent = document.querySelectorAll('.table-wrap');
+    const ownedProjects = document.getElementsByClassName('owner');
+    const memberProjects = document.getElementsByClassName('member');
+
+    projectsBtns.addEventListener('click' function() {
+      if (e.target
+    });
+  }
+})()
--- basecamp/app/javascript/packs/application.js
+++ basecamp/app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("custom/projectsFilter")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,38 +16,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-window.onload = function() {
-  const projectsBtns = document.querySelectorAll('.projects-filter-btn');
-  const projectsContent = document.querySelectorAll('.table-wrap');
-
-  const toggleProjects = () => {
-
-    for (let i = 0; i < projectsBtns.length; i++) {
-
-      projectsBtns[i].addEventListener('click', event => {
-        event.preventDefault();
-        let target = event.target;
-        let tabContent = document.getElementById(target.getAttribute("href"));
-        removeActive(projectsBtns);
-        hideContent(projectsContent);
-        target.classList.add("active");
-        tabContent.classList.remove("hide");
-      });
-    }
-  }
-
-  const removeActive = (array) => {
-    for (let i = 0; i < array.length; i++) {
-      array[i].classList.remove("active");
-    }
-  }
-
-  const hideContent = (array) => {
-    for (let i = 0; i < array.length; i++) {
-      array[i].classList.add("hide");
-    }
-  }
-
-  toggleProjects();
-}
