55a024f41c5ed32428666796edd4889943fa6992
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-06-17 09:04:33 -0700

    fixes

--- basecamp/app/controllers/welcome_controller.rb
+++ basecamp/app/controllers/welcome_controller.rb
@@ -1,30 +1,9 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
-  def getProjectsByMember(projects)
-    projectsArray = []
-    projects.each do |project|
-      project.members.each do |member|
-        if member.email == current_user.email
-          projectsArray.push(project)
-        end
-      end
-    end
-    return projectsArray
-  end
-
-  def concatProjects(array1, array2)
-    projectsAll = array1.concat(array2)
-
-    return projectsAll
-  end
-
   def index
     if user_signed_in?
-      @projectsAll = Project.all
-      @projectsByOwner = Project.where("user_id=?", current_user.id)
-      @projectsByMember = getProjectsByMember(@projectsAll)
-      @projects = concatProjects(@projectsByOwner.to_a, @projectsByMember.to_a)
+      redirect_to projects_path
     end
   end
 end
