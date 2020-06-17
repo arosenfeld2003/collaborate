fd3bb751343976a361d65a4df09fdda813925d13
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-06-16 22:19:53 -0700

    js filter project views member owner all

--- basecamp/app/javascript/custom/projectsFilter.js
+++ basecamp/app/javascript/custom/projectsFilter.js
@@ -1,12 +1,24 @@
 (function() {
   window.onload = function() {
     const projectsBtns = document.querySelectorAll('.projects-filter-btn');
-    const projectsContent = document.querySelectorAll('.table-wrap');
-    const ownedProjects = document.getElementsByClassName('owner');
-    const memberProjects = document.getElementsByClassName('member');
+    const ownedProjects = Array.from(document.getElementsByClassName('owner'));
+    const memberProjects = Array.from(document.getElementsByClassName('member'));
 
-    projectsBtns.addEventListener('click' function() {
-      if (e.target
-    });
+    projectsBtns.forEach(btn => {
+      addEventListener('click', event => {
+        let target = event.target;
+        if (target.id === 'owner') {
+          ownedProjects.forEach(el => el.style.display = "block");
+          memberProjects.forEach(el => el.style.display = "none");
+        } else if (target.id === 'member'){
+          ownedProjects.forEach(el => el.style.display = "none");
+          memberProjects.forEach(el => el.style.display = "block");
+        } else {
+          ownedProjects.forEach(el => el.style.display = "block");
+          memberProjects.forEach(el => el.style.display = "block");
+        }
+      })
+    })
   }
 })()
+
