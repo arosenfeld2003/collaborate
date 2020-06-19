592c6932144361e3c7da49f0a38bf6090664e408
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-06-18 18:33:23 -0700

    fix projects filter bug with turbolinks

--- basecamp/app/javascript/custom/projectsFilter.js
+++ basecamp/app/javascript/custom/projectsFilter.js
@@ -1,23 +1,21 @@
-(function() {
-  window.onload = function() {
-    const projectsBtns = document.querySelectorAll('.projects-filter-btn');
-    const ownedProjects = Array.from(document.getElementsByClassName('owner'));
-    const memberProjects = Array.from(document.getElementsByClassName('member'));
+document.addEventListener('turbolinks:load', function() {
+  var projectsBtns = document.querySelectorAll('.projects-filter-btn');
+  var ownedProjects = Array.from(document.getElementsByClassName('owner'));
+  var memberProjects = Array.from(document.getElementsByClassName('member'));
 
-    projectsBtns.forEach(btn => {
-      addEventListener('click', event => {
-        let target = event.target;
-        if (target.id === 'owner') {
-          ownedProjects.forEach(el => el.style.display = "block");
-          memberProjects.forEach(el => el.style.display = "none");
-        } else if (target.id === 'member'){
-          ownedProjects.forEach(el => el.style.display = "none");
-          memberProjects.forEach(el => el.style.display = "block");
-        } else {
-          ownedProjects.forEach(el => el.style.display = "block");
-          memberProjects.forEach(el => el.style.display = "block");
-        }
-      })
+  projectsBtns.forEach((btn) => {
+    addEventListener('click', event => {
+      let target = event.target;
+      if (target.id === 'owner') {
+        ownedProjects.forEach(el => el.style.display = "block");
+        memberProjects.forEach(el => el.style.display = "none");
+      } else if (target.id === 'member') {
+        ownedProjects.forEach(el => el.style.display = "none");
+        memberProjects.forEach(el => el.style.display = "block");
+      } else {
+        ownedProjects.forEach(el => el.style.display = "block");
+        memberProjects.forEach(el => el.style.display = "block");
+      }
     })
-  }
-})()
+  })
+}, false);
