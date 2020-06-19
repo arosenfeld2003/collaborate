4a429ca5223a2d74a284893e05cce87bef48b553
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-06-18 21:50:31 -0700

    fix

--- basecamp/app/models/user.rb
+++ basecamp/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
-  has_many :projects
+  has_many :projects, dependent: :destroy
 
 end
