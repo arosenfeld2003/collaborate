8ee1426f80e9d3b6d390d78849790c7abdf0bb66
anastaszi <anastasba@gmail.com>
2020-07-05 12:45:04 -0700

    messages and threads blanks

---
+++ basecamp/app/controllers/threads_controller.rb
@@ -0,0 +1,2 @@
+class ThreadsController < ApplicationController
+end
--- basecamp/config/routes.rb
+++ basecamp/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     resources :members do
       delete 'unsubscribe'
     end
+    resources :threads do
+      resources :messages
+    end
   end
 
   devise_for :users
